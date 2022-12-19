f113e7e3f4218205ba6c497d1283c0252987c5ee
MuhammadDeveloper777 <121014727+MuhammadDeveloper777@users.noreply.github.com>
2022-12-19 20:06:57 +0500

    js in calculator

---
+++ js/calculator.js
@@ -0,0 +1,24 @@
+function multipli() 
+{
+	let a=num1.value;
+	let b=num2.value;
+	let c=num3.value;
+	let d=Number(a)*Number(b)*Number(c);
+		result.value=d;
+}
+function divi()
+{
+	let a=num1.value;
+	let b=num2.value;
+	let c=num3.value;
+	 let d=Number(a)/Number(b)/Number(c);
+	 	result.value=d;
+}
+function remai()
+{
+	let a=num1.value;
+	let b=num2.value;
+	let c=num3.value;
+	let d=Number(a)%Number(b)%Number(c);
+		result.value=d
+}
